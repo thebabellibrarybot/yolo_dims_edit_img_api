e79d602ce356c3b7ac36658477bd1d218fbc2661
thebabellibrarybot <jack@thebabellibrary.org>
2023-04-02 13:30:20 -0400

    put all funcs in app.py to make lambda env easier

--- app.py
+++ app.py
@@ -7,9 +7,52 @@
 #    "action": "crop"
 #    }
 #}
+import boto3
+from PIL import Image, ImageDraw
+from io import BytesIO
 
-from edit_img import crop_image
-from edit_img import mask_img
+def crop_image(img, buk, dims):
+    print('edit_img started')
+    s3_img_key = img
+    s3_bucket = buk
+    crop_to_dims = []
+    for classes, dim in dims.items():
+        crop_to_dims.append(dim)
+   
+    out = {}
+    for dim in crop_to_dims:
+
+        # download image to bytes from s3
+        s3 = boto3.client('s3')
+        img_object = s3.get_object(Bucket=s3_bucket, Key=s3_img_key)
+        img_data = img_object['Body'].read()
+        img_pil = Image.open(BytesIO(img_data))
+        img_pil = img_pil.resize((640, 640))
+
+        # mask to 640x640 sz
+        # Create a mask to keep everything inside the bounding box
+        mask = Image.new('L', img_pil.size, color=0)
+        draw = ImageDraw.Draw(mask)
+        draw.rectangle(dim, fill=255)
+
+        # Apply the mask to the image to set everything outside of the bounding box to white
+        masked_image = Image.new('RGBA', img_pil.size, color=(255, 255, 255, 255))
+        masked_image.paste(img_pil, mask=mask)
+        
+        # save and upload actions
+        cl = str(dims.keys()).replace('dict_keys', '').replace('([', '').replace('])', '')
+        cropped_key = 'cropped_' + cl + '_' + s3_img_key
+        img_bytes = BytesIO()
+        masked_image = masked_image.convert('RGB')
+        masked_image.save(img_bytes, format='JPEG')
+        img_bytes.seek(0)
+        cropped_key = cropped_key.replace(' ', '_').replace('+', '%2B').replace(',','_')
+        s3.put_object(Bucket=s3_bucket, Key=cropped_key, Body=img_bytes)
+        out[cropped_key] = {s3_bucket: f'https://{s3_bucket}.s3.amazonaws.com/{cropped_key}'}
+    return out
+
+def mask_img(img, buk, dims):
+    return
 
 def lambda_hanlder(event, context):
     coord_class = event['body']['class']
